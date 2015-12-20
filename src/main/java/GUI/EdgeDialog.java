b1f659ef07036eabbbe1d99f698edd92a97725ee
Yoo Jae Beom <gombabo1217@naver.com>
2015-12-20 10:18:29 +0900

    make some files

---
+++ src/main/java/GUI/EdgeDialog.java
@@ -0,0 +1,9 @@
+package GUI;
+
+import javax.swing.*;
+
+/**
+ * Created by user on 2015-12-20.
+ */
+public class EdgeDialog extends JDialog {
+}
---
+++ src/main/java/GUI/MainFrame.java
@@ -0,0 +1,59 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by user on 2015-12-20.
+ */
+public class MainFrame extends JFrame implements ActionListener{
+    private JPanel mainPanel, resultPanel, buttonPanel;
+    private JButton addVertex, addEdge, show;
+    private TextArea result;
+    
+    public MainFrame(){
+        super("채무 관계 그래프");         //  dept relations graph
+
+        mainPanel = new JPanel();
+        resultPanel = new JPanel();
+        buttonPanel = new JPanel();
+
+        result = new TextArea();
+
+        addVertex = new JButton("인원 추가");
+        addEdge = new JButton("채무 관계 추가");
+        show = new JButton("결과 출력");
+
+        resultPanel.add(result);
+
+        buttonPanel.add(addVertex);
+        buttonPanel.add(addEdge);
+        buttonPanel.add(show);
+        buttonPanel.setLayout(new GridLayout(1, 3));
+
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(resultPanel, "Center");
+        mainPanel.add(buttonPanel, "South");
+
+
+        this.add(mainPanel);
+        this.setSize(400, 300);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource().equals(addVertex)){            //  if addVertex is pressed
+
+        }
+        else if(e.getSource().equals(addEdge)){         //  if addEdge is pressed
+
+        }
+        else{                                            //   if result is pressed
+
+        }
+    }
+}
