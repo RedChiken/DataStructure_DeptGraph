7b9c84b431532bc59cd30e9c9c441eb6079eb42e
lyuha <beingfunnylife@gmail.com>
2015-12-23 03:49:53 +0900

    implement fragmetize method in Optimizer

--- src/main/java/dept/Optimizer.java
+++ src/main/java/dept/Optimizer.java
@@ -4,11 +4,16 @@ import graph.DirectedWeightedGraph;
 import graph.WeightedEdge;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Optimizer {
     private DirectedWeightedGraph<String> graph;
+    HashMap<String, Integer> incomeVertex;
+    HashMap<String, Integer> outcomeVertex;
 
     public Optimizer(DirectedWeightedGraph<String> graph) {
+        incomeVertex = new HashMap<>();
+        outcomeVertex = new HashMap<>();
         try {
             this.graph = (DirectedWeightedGraph<String>) graph.clone();
         } catch (CloneNotSupportedException e) {
@@ -38,7 +43,14 @@ public class Optimizer {
     }
 
     public void fragmentize(){
+        ArrayList<WeightedEdge<String>> edges = this.graph.getEdges();
 
+        edges.forEach(edge-> {
+            outcomeVertex.computeIfPresent(edge.getSource(), (s, integer) -> edge.getWeight() + integer);
+            outcomeVertex.putIfAbsent(edge.getSource(), edge.getWeight());
+            incomeVertex.computeIfPresent(edge.getDestination(), (s, integer) -> edge.getWeight() + integer);
+            incomeVertex.putIfAbsent(edge.getDestination(), edge.getWeight());
+        });
     }
 
     public DirectedWeightedGraph<String> get() {
